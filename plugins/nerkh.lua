9abfa1695fd6f329c7e6bf5a0c466c06d80e6c7e
alimessibot <aliamiralizadebot@gmail.com>
2016-04-30 14:21:40 +0430

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,19 @@
+ do
+
+function run(msg, matches)
+  return "slm dost aziz\nHar super gp (5) Hezar tomn bedone pardakht mahane...\nBaraye khared be admin  TelekillerBot  p.m bedid...\n @Xx_fire_boy_eblis_xX  \n @kos_khahare_zendegi "
+  end
+return {
+  description = " nerkh ",
+  usage = "nerkh",
+  patterns = {
+    "^Nerkh$",
+    "^nerkh$",
+    "^[#$%&/!](nerkh)$",
+    "^[#$%&/!]([Nn]erkh)$"
+  },
+  run = run
+}
+end 
+
+
